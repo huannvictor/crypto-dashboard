ff6b59b2a1f858d9b1e404bb868ac73f42b5f2b3
Huann Almeida <67878933+huannvictor@users.noreply.github.com>
2022-01-10 17:32:59 -0300

    started news feed

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -1,9 +1,33 @@
-const NewsFeed = () => {
-  return(
-    <div className="newsFeed">
-      Here is the news baby!
-    </div>
-  )
-}
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-export default NewsFeed
+const NewsFeed = () => {
+  const [articles, setArticles] = useState(null);
+
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      url: "https://crypto-news15.p.rapidapi.com/news",
+      headers: {
+        "x-rapidapi-host": "crypto-news15.p.rapidapi.com",
+        "x-rapidapi-key": "4f44b2b4famsh9851d3135c14023p1d68aajsn8515efd61176",
+      },
+    };
+
+    axios
+      .request(options)
+      .then(response => {
+        console.log(response.data);
+        setArticles(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
+  console.log(articles);
+
+  return <div className="newsFeed">Here is the news baby!</div>;
+};
+
+export default NewsFeed;
